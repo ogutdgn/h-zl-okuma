c9a9eaa0780b48bfee23de4ed7d41249fe810b37
doan <ogutdgn@gmail.com>
2022-09-24 22:43:46 +0300

    .

--- src/pages/ReadingExercise/ReadingExercise.js
+++ src/pages/ReadingExercise/ReadingExercise.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../../components/Layout/Layout';
 import "./ReadingExercise.css";
 import { useParagraphStore } from '../../store/useParagraphStore';
+import ReadingExerciseBtns from './ReadingExerciseBtns';
 
 const ReadingExercise = () => {
   //const wordArray = ["naber", "lebit", "esad", "doan", "ğhahaha"];
 
   const { currentParagraph, changeParagraphFunc, wordsInParagraph, setWordsInParagraph } = useParagraphStore();
   const [currentWord, setCurrentWord] = useState(wordsInParagraph[0]);
+  const [wordIndex, setWordIndex] = useState(0);
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
 
   useEffect(() => {
     setWordsInParagraph(currentParagraph);
@@ -16,14 +20,47 @@ const ReadingExercise = () => {
 
 
 
+  // const handleStart = () => {
+  //   for (let i = 0; i < wordsInParagraph.length; i++) {
+  //     setTimeout(() => {
+  //       setCurrentWord(wordsInParagraph[i]);
+  //     }, 100 * i);
+  //   }
+  // }
+
+  let currentWordIndex = wordIndex;
   const handleStart = () => {
-    for (let i = 0; i < wordsInParagraph.length; i++) {
-      setTimeout(() => {
-        console.log(wordsInParagraph[i]);
-        setCurrentWord(wordsInParagraph[i]);
-      }, 100 * i);
-    }
+    setCurrentWord(wordsInParagraph[currentWordIndex]);
+    currentWordIndex++;
+    return setWordIndex(currentWordIndex);
   }
+
+  const start = () => {
+    handleStart();
+    
+    setInterv(setInterval(handleStart, 100));
+    setStatus(1);
+  }
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  }
+
+  const reset = () => {
+    clearInterval(interv);
+    setWordIndex(0);
+    setCurrentWord(wordsInParagraph[0]);
+    setStatus(0);
+  }
+
+  const resume = () => {
+    start();
+  }
+
+  // const startText = () => {
+  //   setInterv(setInterval(handleStart, 100))
+  // }
 
   // wordArray.forEach((e) => {
   //   for (let i = 0; i < wordArray.length; i++) {
@@ -37,12 +74,16 @@ const ReadingExercise = () => {
 
   return (
     <Layout>
-        <div className="wordBox">
-            <div className="word">
-              <p>{currentWord}</p>
-            </div>
+        <div className="spreeder">
+          <div className="wordBox">
+              <div className="word">
+                <p>{currentWord}</p>
+              </div>
+          </div>
+          <ReadingExerciseBtns status={status} start={start} stop={stop} reset={reset} resume={resume}/>
         </div>
-        <button onClick={handleStart}>Go</button>
+        {/* <button onClick={runWords}>Go</button>
+        <button onClick={stopWords}>Stop</button> */}
     </Layout>
   )
 }
---
+++ src/pages/ReadingExercise/ReadingExerciseBtns.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const ReadingExerciseBtns = (props) => {
+    return (
+        <div className="buttons">
+          <div className="startBtn">
+            {(props.status === 0)? 
+              <Button 
+              className="stopwatch-btn stopwatch-btn-gre"
+              variant="contained"
+              style={{ width: "100px", backgroundColor: "green" }}
+              onClick={props.start}
+              >
+                Başla
+              </Button> : ""
+            }
+          </div>
+    
+          <div className="stopResetBtn">
+            {(props.status === 1)? 
+              <div>
+                <Button 
+                className="stopwatch-btn stopwatch-btn-red"
+                variant="contained"
+                style={{ width: "100px", backgroundColor: "red" }}
+                onClick={props.stop}
+                >
+                  Durdur
+                </Button>
+                
+              </div> : ""
+            }
+          </div>
+    
+         <div className="resumeFinishBtn">
+          {(props.status === 2)? 
+              <div>
+                <Button 
+                className="stopwatch-btn stopwatch-btn-gre"
+                variant="contained"
+                style={{ width: "100px", backgroundColor: "green" }}
+                onClick={props.resume}
+                >
+                  Devam
+                </Button>
+                <Button 
+                className="stopwatch-btn stopwatch-btn-yel"
+                variant="contained"
+                style={{ width: "100px" }}
+                onClick={props.reset}
+                >
+                  Sıfırla
+                </Button>
+              </div> : ""
+            }
+         </div>
+         
+        </div>
+      );
+}
+
+export default ReadingExerciseBtns;
