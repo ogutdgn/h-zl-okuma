d7792a33f25e886c30d619b0232a574817ce8100
doan <ogutdgn@gmail.com>
2023-10-18 14:03:42 +0300

    .

--- src/components/KeyPressEvent/KeyPressEvent.js
+++ src/components/KeyPressEvent/KeyPressEvent.js
@@ -2,41 +2,34 @@ import { useEffect } from 'react';
 import { useParagraphStore } from '../../store/useParagraphStore';
 
 export const KeyPressEvent = (callBack, callBack2, targetKey) => {
-
     const { startSpotWatch, setStartSpotWatch, activeElement } = useParagraphStore();
-    //const [isFocus, setIsFocus] = useState(false);
-    //console.log(document.selection);
-    
 
     useEffect(() => {
-        //let text = document.getElementById("text");
-        // text = document.activeElement;
-        //text === document.activeElement ? setIsFocus(true) : setIsFocus(false);
-
-        //console.log(isFocus);,
-        //console.log(activeElement);
-
         const keyPressHandler = (e) => {
-            
-            if((activeElement === false) && (e.key === targetKey)){
-                if((startSpotWatch === false)){
-                    callBack();
-                    setStartSpotWatch(true);
-                }else{
+            if (activeElement === false && e.key === targetKey) {
+                if (startSpotWatch === false) {
+                    // Check if the active element is an input or textarea
+                    const isInputActive = document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA';
+                    
+                    if (!isInputActive) {
+                        callBack();
+                        setStartSpotWatch(true);
+                    }
+                } else {
                     callBack2();
                     setStartSpotWatch(false);
                 }
-                    
             }
-            
         }
-        if(activeElement === false){
+
+        if (activeElement === false) {
             window.addEventListener("keydown", keyPressHandler);
         }
+        
         return () => {
             window.removeEventListener("keydown", keyPressHandler);
         }
     }, [callBack, callBack2, targetKey, startSpotWatch, setStartSpotWatch, activeElement]);
-
 }
+
 
