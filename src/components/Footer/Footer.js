8567a2a8681888ec12e3fe9ca40c87e749845fd3
doan <ogutdgn@gmail.com>
2023-10-22 02:28:53 +0300

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,42 +1,59 @@
 import React from 'react';
-import './Footer.css';
+import { Container, Grid, Typography, Box, Link, IconButton } from '@mui/material';
+import { Facebook, Twitter, Instagram, LinkedIn, Phone, Email, Send } from '@mui/icons-material';
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div style={{ width: "1200px", display: "flex" }}>
-      <section className="footer-about">
-        <h2>ABOUT US</h2>
-        <p>StemWise is where young minds embark on a journey of innovation. Through fun programming lessons and mentorship, we're nurturing the next generation as problem solvers, dreamers, and trailblazers in the world of STEM for a brighter, smarter future.</p>
-        <div className="footer-icons">
-          {/* Replace with your icons */}
-          <i className="icon-instagram"></i>
-          <i className="icon-envelope"></i>
-          <i className="icon-phone"></i>
-          <i className="icon-linkedin"></i>
-          <i className="icon-youtube"></i>
-        </div>
-      </section>
-      <section className="footer-news">
-        <h2>LATEST NEWS</h2>
-        {/* Sample News List */}
-        <p>13 Sep - Neuroscience on the Slopes: How Skiing Enhances Brain Function and Motor Neurons</p>
-        <p>13 Oct - Just another post with A Gallery</p>
-        <p>13 Oct - A Simple Blog Post</p>
-        <p>01 Jan - A Video Blog Post</p>
-      </section>
-      <section className="footer-categories">
-        <h2>CATEGORIES</h2>
-        {/* Sample Categories List */}
-        <p>Programming</p>
-        <p>Science</p>
-      </section>
-      </div>
-      <div className="footer-bottom">
-        <p>ABOUT | OUR STORES | BLOG | CONTACT | FAQ</p>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <Box sx={{ bgcolor: '#2C2C2C', color: 'white', py: 4 }}> {/* Increased vertical padding */}
+                <Container>
+                    <Grid container spacing={4}> {/* Increased grid spacing */}
+                        <Grid item xs={12} md={6} lg={5}>
+                            <Typography variant="h5" component="div" sx={{ fontWeight: 'bold' }}><span>WALEED</span>CODES</Typography>
+                            <Typography sx={{ fontWeight: 'bold', mt: 2 }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit...</Typography> {/* Increased margin-top */}
+                            <Box mt={3}> {/* Increased margin-top */}
+                                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                                    <IconButton color="inherit" size="large"><Facebook /></IconButton>
+                                </a>
+                                <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
+                                    <IconButton color="inherit" size="large"><Twitter /></IconButton>
+                                </a>
+                                <a href="https://www.instagram.com/swift_readers" target="_blank" rel="noreferrer">
+                                    <IconButton color="inherit" size="large"><Instagram /></IconButton>
+                                </a>
+                                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+                                    <IconButton color="inherit" size="large"><LinkedIn /></IconButton>
+                                </a>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={6} lg={3}>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>Quick Links</Typography>
+                            <Box mt={3}> {/* Increased margin-top */}
+                                <Link href="/services" color="inherit" display="block" sx={{ textDecoration: 'none', my: 1, fontSize: '1.1rem', fontWeight: 'bold', '&:hover': { textDecoration: 'underline', color: "white" } }}>Services</Link>
+                                <Link href="/about" color="inherit" display="block" sx={{ textDecoration: 'none', my: 1, fontSize: '1.1rem', fontWeight: 'bold', '&:hover': { textDecoration: 'underline', color: "white" } }}>About</Link>
+                                <Link href="/contact" color="inherit" display="block" sx={{ textDecoration: 'none', my: 1, fontSize: '1.1rem', fontWeight: 'bold', '&:hover': { textDecoration: 'underline', color: "white" } }}>Contact</Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={6} lg={4}>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>Contact</Typography>
+                            <Box mt={3}> {/* Increased margin-top */}
+                                <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}><Phone color="inherit" sx={{ mr: 1 }} />+90 5445629942</Typography>
+                                <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}><Link href="mailto:ogutdgn@swiftreaders.com" sx={{ color: "white", textDecoration: 'none', '&:hover': { textDecoration: 'underline', color: "white" } }}><Email color="inherit" sx={{ mr: 1 }} />ogutdgn@swiftreaders.com</Link></Typography>
+                                <Typography variant="body1" sx={{ fontWeight: 'bold' }}><Send color="inherit" sx={{ mr: 1 }} /> Ankara, Turkey.</Typography>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+            <Box sx={{ bgcolor: '#2C2C2C', color: 'white', py: 3 }}> {/* Increased vertical padding */}
+                <Container>
+                    <Typography align="center" variant="body2" sx={{ fontWeight: 'bold' }}>
+                        © 2023 All rights reserved. | Design By Dastugo Technology | <Link href="/" color="inherit" sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>Terms & Policy</Link>
+                    </Typography>
+                </Container>
+            </Box>
+        </>
+    )
 }
 
 export default Footer;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,99 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from 'react';
+import {
+  Box, Typography, Avatar, IconButton
+} from '@mui/material';
+import { Facebook, Twitter, Instagram } from '@mui/icons-material';
+import Layout from '../../components/Layout/Layout';
+
+const teamMembers = [
+  { name: 'Tom Cruise', role: 'Full Stack Developer', img: './man.jpg' },
+  { name: 'David Wornar', role: 'Front End Developer', img: './man.jpg' },
+  { name: 'Vin Diesel', role: 'Back End Developer', img: './man.jpg' }
+];
+
+const TeamMemberCard = ({ name, role, img }) => {
+  return (
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        borderRadius: '50%',
+        m: 2,
+        p: 3,
+        width: 220,
+        height: 220,
+        transition: 'all .4s',
+        '&:hover': {
+          borderRadius: '10%',
+          height: 260,
+        }
+      }}
+    >
+      <Avatar
+        src={img}
+        alt={name}
+        sx={{
+          width: '100%',
+          height: '100%',
+          transition: 'all .4s',
+          borderRadius: '50%',
+          '&:hover': {
+            transform: 'translateY(-70px)',
+            borderRadius: '10%',
+          }
+        }}
+      />
+      <Box
+        sx={{
+          textAlign: 'center',
+          transform: 'translateY(-90px)',
+          opacity: 0,
+          transition: 'all .5s',
+          '&:hover': {
+            opacity: 1,
+          }
+        }}
+      >
+        <Typography variant="h6" component="div" color="primary">
+          {name}
+        </Typography>
+        <Typography variant="subtitle1" component="div">
+          {role}
+        </Typography>
+        <Box>
+          <IconButton>
+            <Facebook color="action" />
+          </IconButton>
+          <IconButton>
+            <Twitter color="action" />
+          </IconButton>
+          <IconButton>
+            <Instagram color="action" />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 const About = () => {
   return (
     <Layout>
-      About
+      <Box
+        sx={{
+          width: '100%',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bgcolor: '#0c52a1'
+        }}
+      >
+        {teamMembers.map(member => (
+          <TeamMemberCard key={member.name} {...member} />
+        ))}
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
