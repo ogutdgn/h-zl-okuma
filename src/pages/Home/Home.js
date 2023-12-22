10c8af93293968dec8b6c3e7e7871a24b079e4f1
doan <ogutdgn@gmail.com>
2023-12-22 14:44:32 +0300

    .

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,6 @@ import { styled } from '@mui/system';
 import Layout from '../../components/Layout/Layout';
 import libraryImage from "../../assets/imgs/book1.jpeg";
 import Carousel from './Carousel';
-import axios from 'axios';
 
 const CommonStyles = {
   maxWidth: '90%',
@@ -29,23 +28,6 @@ const ImageBackground = styled(Box)({
 });
 
 const Home = () => {
-  const options = {
-    method: 'GET',
-    url: 'https://api.shopier.com/v1/products',
-    headers: {
-      accept: 'application/json',
-      authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJjZjI3MDhlZmJhODQxMzUzNjc1YjExYWEwODNlNDA3NyIsImp0aSI6IjU4ODdkY2JjOTZiZDExYTU5ZWM3NmE3Mzk3MmQ3OTIxYjI5ZmNlZDBkZjczOGM3OTQ3M2VlMTE3ODI0MjQzMzYwMGRmYmNiYTcyOTg4NjJjNjU3YjNmMTFjZGYzZTIzNzI3YWMxMWM5MWVhODFhYWExYTcyNWVjMWM3MmE0OTgyZjA0MDRiNTExZTkwNmIyYTM1MzU5M2E3OTM4NGMzYTkiLCJpYXQiOjE3MDMwNzE0ODQsIm5iZiI6MTcwMzA3MTQ4NCwiZXhwIjoxODYwODU2MjQ0LCJzdWIiOiIxNjE5ODcxIiwic2NvcGVzIjpbIm9yZGVyczpyZWFkIiwib3JkZXJzOndyaXRlIiwicHJvZHVjdHM6cmVhZCIsInByb2R1Y3RzOndyaXRlIiwic2hpcHBpbmdzOnJlYWQiLCJzaGlwcGluZ3M6d3JpdGUiLCJkaXNjb3VudHM6cmVhZCIsImRpc2NvdW50czp3cml0ZSIsInBheW91dHM6cmVhZCIsInJlZnVuZHM6cmVhZCIsInJlZnVuZHM6d3JpdGUiLCJzaG9wOnJlYWQiLCJzaG9wOndyaXRlIl19.M6I46TaWDSobY_9ceR9ixNKH1UTDMYran2fTvUVMw022fvqfhMzwaL5z419saySTzlEmsOE87RWfceMwE2oPKrRQEmZrsWp5tXqmUoXJ-g2uf8n47fGCvQN0pOKDTn3l24WCDnxOaxMcaR5bR7iHHI7cSKQ6NRGRwlUmZ7878ZfS_y3G3fyQTinhXicOvFBlCf_wrKWpLlLFVrsKtBgmoYcIG1dtgwQbc5tqvxU7laodFyhuaawqCscdjGCR1KOSBt47FT_2aN8lNSBfpMYLqOIM7vr9dVsE_cCwr4JrQULRdPeJc0wxY6zfCmLW7TMTO-EqJA31jbVpneAyFR6qpw'
-    }
-  };
-  
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
   return (
     <Layout>
       <Container>
